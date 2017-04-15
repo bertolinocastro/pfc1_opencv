dd5a99651575e397ab4a64098dd26a168842b7a1
bertolinocastro <jgabbc@hotmail.com>
2017-04-15 20:47:15 -0300

    Added Makefile

---
+++ hello_world/main.cpp
@@ -0,0 +1,16 @@
+#include "main.hpp"
+
+
+using namespace std; // Namespace of Standard CPP libs
+using namespace cv;	 // Namespace of OpenCV
+
+int main(int argc, char const *argv[])
+{
+	
+	//Mat frame = cvQueryFrame( capture );
+	//imshow( "Video", frame );
+
+	Mat img1( 3, 5, CV_32F );
+
+	return 0;
+}
